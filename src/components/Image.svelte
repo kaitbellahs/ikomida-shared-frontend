666c13e52163537fc861dc10d277eca650de28e2
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-10-07 03:01:01 -0300

    Add image component Update Button component Update shared libs

---
+++ src/components/Image.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let source = ''
+  export let name = ''
+  export let height = ''
+  export let width = ''
+
+  let showImage = true
+  function erroLoadImage() {
+    showImage = false
+  }
+</script>
+
+{#if showImage && source}
+  <img
+    on:error={() => erroLoadImage()}
+    src={source}
+    alt={name}
+    style="{height ? `height:${height};` : ''}{width ? `width:${width};` : ''}"
+  />
+{/if}
+
+<style>
+  img {
+    width: 100%;
+    max-width: 100%;
+    object-fit: contain;
+  }
+</style>
