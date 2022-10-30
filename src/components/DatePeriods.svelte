202fe475cd8236424018880b48db067e1bbec404
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-10-30 14:06:03 -0300

    Add DatePeriods
    Format code
    Update shared logics lib

---
+++ src/components/DatePeriods.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+  import { Classes, TTextEdit } from '../Types'
+  import Button from './Button.svelte'
+  import Checkbox from './Checkbox.svelte'
+  import Divider from './Divider.svelte'
+  import TextEdit from './TextEdit.svelte'
+  import { v4 as uuid } from 'uuid'
+  import FloatRemove from './FloatRemove.svelte'
+  import Fa from 'svelte-fa'
+  import { faClock } from '@fortawesome/free-solid-svg-icons'
+  import { onMount } from 'svelte'
+
+  interface IDay {
+    name: string
+    checked: boolean
+  }
+  export let business: Classes.CBusinessTime = Classes.CBusinessTime.fromObject({
+    days: [],
+    hours: []
+  })
+  export let title: string | undefined
+  export let mandatory = false
+
+  const days: IDay[] = [
+    { name: 'Domingo', checked: false },
+    { name: 'Segunda-feira', checked: false },
+    { name: 'Terça-feira', checked: false },
+    { name: 'Quarta-feira', checked: false },
+    { name: 'Quinta-feira', checked: false },
+    { name: 'Sexta-feira', checked: false },
+    { name: 'Sabado', checked: false }
+  ]
+
+  $: if (days) {
+    business.days = []
+    for (let index = 0; index < days.length; index++) {
+      if (days[index].checked) {
+        business?.days?.push(index)
+      }
+    }
+  }
+
+  const addHours = () => {
+    if (business) {
+      if (!business?.hours) {
+        business.hours = []
+      }
+      business.hours.push(Classes.CBusinessTimeHours.fromObject({ id: uuid(), start: '08:00', end: '23:59' }))
+      business.hours = business?.hours
+    }
+  }
+
+  function onRemoveClick(id?: string) {
+    if (business) {
+      business.hours = business?.hours?.filter(businessHour => businessHour.id !== id)
+    }
+  }
+  onMount(() => {
+    for (const index of business?.days ?? []) {
+      days[index].checked = true
+    }
+  })
+</script>
+
+<h2>{title ? title : 'horário de funcionamento'}</h2>
+{#if (business?.hours?.length ?? 0) > 0}
+  {#each business?.hours ?? [] as businessHour}
+    <div class="busninessHours">
+      <FloatRemove callback={() => onRemoveClick(businessHour.id)} />
+      <div class="twoCells">
+        <TextEdit
+          placeHolder="Abertura"
+          initialValue={businessHour.start}
+          bind:value={businessHour.start}
+          type={TTextEdit.TIME}
+          rightPadding={10}
+        />
+        <TextEdit
+          placeHolder="Fechamento"
+          bind:value={businessHour.end}
+          initialValue={businessHour.end}
+          type={TTextEdit.TIME}
+          leftPadding={10}
+        />
+      </div>
+    </div>
+  {/each}
+{:else if mandatory}
+  <Divider />
+  <span>Você precisa definir seus {title ? title : 'horário de funcionamento'}</span>
+{/if}
+<Divider />
+<Button on:click={addHours}><Fa icon={faClock} /><span>Adicionar horários</span></Button>
+<div class="days">
+  {#each days as day}
+    <div class="day">
+      <Checkbox marginTop={0} bind:checked={day.checked} label={day.name} />
+    </div>
+  {/each}
+</div>
+
+<style>
+  .busninessHours {
+    position: relative;
+  }
+  .days {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  .days > .day {
+    flex: 1;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin: 5px;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    flex-basis: 40%;
+    text-shadow: 0.5px 1px #18056b66;
+    box-shadow: 1px 1.5px #00000099;
+  }
+</style>
