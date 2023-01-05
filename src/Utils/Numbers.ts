524659543c64a6c144fbcd097ec9871ae076bb6b
khalid ait bellahs <khalid@tialtonivel.com.br>
2023-01-05 13:41:40 -0300

    Bug fix

--- src/Utils/Numbers.ts
+++ src/Utils/Numbers.ts
@@ -1,7 +1,8 @@
 import { Finances } from "@ikomida/shared-logics"
+import type { Classes } from "@ikomida/shared-types"
 import type CCart from "../Types/CCart"
 
-export function calcProductPrice(product: CCart) {
+export function calcProductPrice(product: CCart | Classes.CProduct) {
     let calcTotal = 0
     for (const option of product.options ?? []) {
         calcTotal += product.quantity * option.units * option.price
